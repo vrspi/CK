de9095a6b1196e84969f49dc18ea569a513247d2
Khaireddine Dalaa <53494644+vrspi@users.noreply.github.com>
2023-04-19 11:04:38 +0000

    khaireddine

---
+++ chatkay/src/main/java/CodePackage/CodeWhiz.java
@@ -0,0 +1,9 @@
+package CodePackage;
+
+
+public class CodeWhiz {
+    
+
+
+
+}
---
+++ chatkay/src/main/java/CodePackage/pipline.java
@@ -0,0 +1,28 @@
+package CodePackage;
+
+import java.util.Properties;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+public class pipline {
+    
+    private static StanfordCoreNLP stanfordCoreNLP;
+    private static Properties properties;
+    private static String PropertiesName = "tokenize, ssplit, pos";
+    
+
+    private pipline(){
+
+    }
+    static{
+        properties = new Properties();
+        properties.setProperty("annotators", PropertiesName);
+    }
+
+    public static StanfordCoreNLP getPipline(){
+        if(stanfordCoreNLP == null){
+            stanfordCoreNLP = new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+    }
+}
