0d06f9f161e328e4cb3fdab630f218b4ce65c79d
vrspi <53494644+vrspi@users.noreply.github.com>
2023-04-19 10:45:02 +0000

    Khaireddine

---
+++ chatkay/src/main/java/MathPackage/MathMaven.java
@@ -0,0 +1,6 @@
+package MathPackage;
+
+public class MathMaven {
+    public String Query;
+    
+}
--- chatkay/src/main/java/kay/App.java
+++ chatkay/src/main/java/kay/App.java
@@ -1,9 +1,5 @@
 package kay;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
