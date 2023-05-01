602415a97f4659f432f052216e5ab1b0efb7c4d4
Khaireddine Dalaa <53494644+vrspi@users.noreply.github.com>
2023-05-01 05:01:27 +0100

    khaireddine

--- chatkay/src/main/java/CodePackage/CodeWhiz.java
+++ chatkay/src/main/java/CodePackage/CodeWhiz.java
@@ -17,11 +17,26 @@ public class CodeWhiz {
     private String UserAnswer;
     private List<String> programmingKeywords;
     private String programmingPattern;
+    private String greetingsPattern;
+    private String wellbeingPattern;
+    private Matcher wellbeingMatcher;
+    private Pattern wellbeingRegex;
+    public void setWellbeingPattern(String wellbeingPattern) {
+        this.wellbeingPattern = wellbeingPattern;
+    }
+
+    public void setGreetingsPattern(String greetingsPattern) {
+        this.greetingsPattern = greetingsPattern;
+    }
+    private Matcher greetingsMatcher;
+    private Pattern greetingsRegex;
     private Matcher programmingMatcher;
     private Matcher stepsMatcher;
     private Pattern programmingRegex;
     private Pattern stepsRegex;
     private String stepsPattern;
+    private boolean IsItCode = false;
+    private boolean IsItRequest = false;
     public String getProgrammingPattern() {
         return programmingPattern;
     }
@@ -50,28 +65,65 @@ public class CodeWhiz {
         setStepsPattern(".*\\b(step(s)?|process(es)?|procedure(s)?|guide(s)?|instruction(s)?|manual(s)?|walkthrough(s)?|tutorial(s)?|how\\s(to)?|setup(s)?|configuration(s)?|installation(s)?|operation(s)?|workflow(s)?|task(s)?|flowchart(s)?|map(s)?|checklist(s)?)\\b.*");
         this.programmingRegex = Pattern.compile(programmingPattern, Pattern.CASE_INSENSITIVE);
         this.stepsRegex = Pattern.compile(stepsPattern, Pattern.CASE_INSENSITIVE);
+        setGreetingsPattern("\\b(hi|hello|hey|hey\\sthere|hey\\sguys|hey\\sfolks|hi\\sthere|hi\\sguys|hi\\sfolks|hey\\sho|hey\\syou|hey\\sall|hey\\s[^\\n]+\\b|hi\\sho|hi\\sy'all|hi\\syou|hi\\s[^\\n]+\\b|good\\s(morning|afternoon|evening|night)|greetings|what's\\sup)\\b([^\\n]+)?$");
+        this.greetingsRegex = Pattern.compile(greetingsPattern, Pattern.CASE_INSENSITIVE);
+        setWellbeingPattern(".*\\b(how\\sare\\syou|how's\\sit\\sgoing|how\\sare\\syou\\sdoing|how\\sdo\\syou\\sfeel|how\\sare\\sthings|how\\sis\\severything|what's\\snew|what's\\sup|what's\\sgoing\\son|what's\\shappening|what\\sare\\syou\\supto|what\\sare\\syou\\sworking\\son|how\\sare\\syou\\stoday|how\\sdo\\syou\\sfind\\severything|what\\sis\\sgoing\\son|how\\syou\\sbeen|how\\shave\\syou\\sbeen)\\b(\\s.+)?");
+        this.wellbeingRegex = Pattern.compile(wellbeingPattern, Pattern.CASE_INSENSITIVE);
     }
     public void Start(){
-        boolean IsItCode = false;
-        boolean IsItRequest = false;
-        Scanner sc = new Scanner(System.in);
+        
+      
         System.out.println("CodeWhiz : Welcome to KAY CodeWhiz Mr/Mme."+Username);
         System.out.println("CodeWhiz : How can I assist you?");
-        while(!IsItCode && !IsItRequest){
-           
-            System.out.print(Username+" : ");
-            UserAnswer = sc.nextLine();
-            IsItCode = CheckIfCode(UserAnswer);
-            IsItRequest = CheckIfRequest(UserAnswer);
-            if(!IsItCode){
-                System.out.println("CodeWhiz : Query not about programming topic!");
-                System.out.println("CodeWhiz : Try again!");
-
-            }
-        }
-        RecheckQuery(UserAnswer);
-        sc.close();
+        Giveword();
+        switch (RecheckQuery(UserAnswer)) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case -1:
+            
+            IsItCode = false;
+            IsItRequest = false;            
+            Giveword();
+                break;
+            
+            default:
+                break;
+        } ;
+      
         
+    }
+    private boolean CheckIfGreetings(String Query) {
+        this.greetingsMatcher = greetingsRegex.matcher(Query);
+        this.wellbeingMatcher = wellbeingRegex.matcher(Query);
+        int x=0;
+            if(greetingsMatcher.matches()){
+                System.out.println("CodeWhiz : Hi");
+                
+                x=1;
+                
+                
+            }
+            if(wellbeingMatcher.matches()){
+                
+                if(x==1)
+                System.out.println("I'm doing well thank you!");
+
+                else{
+                    x=1;
+                    System.out.println("CodeWhiz : I'm doing well thank you!");
+                }
+                
+            }
+            if(x==1)
+            {
+                System.out.println("CodeWhiz : How can I assist you?");
+                return true;
+            }
+            return false;
     }
     private boolean CheckIfCode(String Query){
         List<String> programmingKeywords = getProgrammingKeywords();
@@ -107,17 +159,38 @@ public class CodeWhiz {
 
         return programmingKeywords;
     }
-    private void RecheckQuery(String Query){
+    private int RecheckQuery(String Query){
         this.programmingMatcher = programmingRegex.matcher(Query);
         this.stepsMatcher = stepsRegex.matcher(Query);
         if (programmingMatcher.matches()) {
-            System.out.println("The user is asking about programming.");
+             System.out.println("The user is asking about programming.");
+            return 1;
+
         } else if (stepsMatcher.matches()) {
-            System.out.println("The user is asking about step-by-step processes.");
+             System.out.println("The user is asking about step-by-step processes.");
+            return 2;
         } else {
-            System.out.println("The query is not related to programming or steps.");
+             System.out.println("The query is not related to programming or steps.");
+            return -1;
         }
     }
-
+    private void Giveword(){
+        Scanner sc = new Scanner(System.in);
+       
+        while(!IsItCode && !IsItRequest){  
+            System.out.print(Username+" : ");
+            UserAnswer = sc.nextLine();
+            if(CheckIfGreetings(UserAnswer)){
+                continue;
+            }
+            IsItCode = CheckIfCode(UserAnswer);
+            IsItRequest = CheckIfRequest(UserAnswer);
+            if(!IsItCode && !IsItRequest){
+                System.out.println("CodeWhiz : Your questiion is not about any programming topic!");
+                System.out.println("CodeWhiz : please try again!");
+            }
+        }
+        sc.close();
+    }
 
 }
